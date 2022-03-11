e31aa10a0bdae307b7cfeb1edca278fd017c0bc2
Luis Mercado <mercadoluisdaniel@gmail.com>
2022-03-10 22:38:15 -0600

    Updates

--- src/containers/Todos/store/selectors.ts
+++
@@ -1,6 +0,0 @@
-import { createSelector } from 'reselect';
-import type { IState } from './index';
-
-const todosState = (state: IState) => state.todos;
-
-export const getTodos = createSelector(todosState, (state) => state.todos);
